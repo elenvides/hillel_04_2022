427ca386804b00ead7a45551075509f1f26bd3a7
elenvides <35038295+elenvides@users.noreply.github.com>
2023-05-10 17:35:16 +0300

    hw7: threads, processes (#6)
    * hw7: threads, processes
    
    * update
    
    * img removed

---
+++ lesson_09_hw_07/threads_and_processes.py
@@ -0,0 +1,55 @@
+import os
+import threading
+from multiprocessing import Process
+from time import perf_counter
+
+import requests
+
+
+# CPU-bound task (heavy computation)
+def encrypt_file(path: str):
+    print(f"Processing file from {path} in process {os.getpid()}")
+    # Simulate heavy computation by sleeping for a while
+    _ = [i for i in range(100_000_000)]
+
+
+# I/O-bound task (downloading image from URL)
+def download_image(image_url):
+    print(
+        f"Downloading image from {image_url} in thread {threading.current_thread().name}"
+    )
+    response = requests.get(image_url)
+    with open("image.jpg", "wb") as f:
+        f.write(response.content)
+
+
+if __name__ == "__main__":
+    try:
+        encryption_task = Process(target=encrypt_file, args=("rockyou.txt",))
+        download_task = threading.Thread(
+            target=download_image, args=("https://picsum.photos/1000/1000",)
+        )
+
+        total_start = perf_counter()
+
+        encryption_start = perf_counter()
+        encryption_task.start()
+
+        download_start = perf_counter()
+        download_task.start()
+
+        download_task.join()
+        download_counter = perf_counter() - download_start
+
+        encryption_task.join()
+        encryption_counter = perf_counter() - encryption_start
+
+        total_counter = perf_counter() - total_start
+
+        print(
+            f"Time taken for encryption task: {encryption_counter}, "
+            f"I/O-bound task: {download_counter}, "
+            f"Total: {total_counter} seconds"
+        )
+    except Exception as e:
+        print(f"Error occurred: {e}")
